7be310c58bb7fc2b79dc2cfcfddd12afc8a68e54
OlZh6262 <2701oleg2701@gmail.com>
2022-08-13 10:18:22 +0200

    release v2

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Mykyta Bobkov',
+    age: 20,
+    image:
+      'https://res.cloudinary.com/dij2i9gth/image/upload/v1660327954/Birthday-reminder/photo_2017-09-04_21-06-20_obtryr.jpg',
+  },
+  {
+    id: 2,
+    name: 'Yaroslav Strelchuk',
+    age: 19,
+    image:
+      'https://res.cloudinary.com/dij2i9gth/image/upload/v1660374509/Birthday-reminder/Smiling-Cat_oifaia.jpg',
+  },
+  {
+    id: 3,
+    name: 'Vladyslav Omelchenko',
+    age: 18,
+    image:
+      'https://res.cloudinary.com/dij2i9gth/image/upload/v1660328213/Birthday-reminder/photo_2022-06-04_19-58-33_tysbze.jpg',
+  },
+  {
+    id: 4,
+    name: 'Mykola Nosov',
+    age: 19,
+    image:
+      'https://res.cloudinary.com/dij2i9gth/image/upload/v1660328573/Birthday-reminder/photo_2022-05-28_22-21-01_c8equt.jpg',
+  },
+  {
+    id: 5,
+    name: 'Oleh Zholubak',
+    age: 18,
+    image:
+      'https://res.cloudinary.com/dij2i9gth/image/upload/v1660329100/Birthday-reminder/photo_2022-07-03_23-13-21_ih6toe.jpg',
+  },
+];
